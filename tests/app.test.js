89301d3c051c49c657ce2f1eb42a21733dd92024
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-09-17 15:24:44 +0100

    updated test

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,6 +5,6 @@ import app from '../src/app';
 describe('Test app for setup', () => {
   it('Testing ', async () => {
     const { body: { data } } = await request(app).get('/');
-    expect(data).toMatch(/respond with a resource/);
+    expect(data).toMatch(/respond with a resource at endpoint root/);
   });
 });
