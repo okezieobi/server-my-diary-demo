fb4fcf3c5e8133575733c8cf1f97c94301c44747
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-10-08 12:59:54 +0100

    Added more tests for user signup

--- tests/user.test.js
+++ tests/user.test.js
@@ -5,7 +5,14 @@ import utils from './utils';
 
 describe('User should be able to signup to the app', () => {
   it('User should be able to successfully signup with all required fields ', async () => {
-    const { status } = await request(app).post('/api/v1/auth/signup').send(utils.user.mock);
+    const { status, body: { data } } = await request(app).post('/api/v1/auth/signup').send(utils.user.mock);
     expect(status).toBeNumber().toEqual(201);
+    expect(data).toBeObject().toContainKeys(['user', 'token']);
+    expect(data.token).toBeString();
+    expect(data.user.fullName).toBeString().toEqual(utils.user.mock.fullName);
+    expect(data.user.username).toBeString().toEqual(utils.user.mock.username);
+    expect(data.user.email).toBeString().toEqual(utils.user.mock.email);
+    expect(data.user.type).toBeString().toEqual('Client');
+    expect(data.user.createdAt).toBeString();
   });
 });
