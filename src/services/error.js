998402372b68335868dd9076ded137215cd07589
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2021-03-14 02:15:36 +0100

    Test fixauth 11

--- src/services/error.js
+++ src/services/error.js
@@ -2,7 +2,6 @@ export default class CustomError extends Error {
   constructor(status = 400, ...params) {
     // Pass remaining arguments (including vendor specific ones) to parent constructor
     super(...params);
-
     // Maintains proper stack trace for where our error was thrown (only available on V8)
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, CustomError);
