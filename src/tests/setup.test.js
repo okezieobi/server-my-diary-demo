472cbf879fef6cf4ba0b760f3ce7748fc4325b3f
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2021-03-19 21:06:19 +0100

    Rese jest fix

--- src/tests/setup.test.js
+++ src/tests/setup.test.js
@@ -3,8 +3,6 @@ import 'jest-extended';
 
 import utils from './utils';
 
-jest.setTimeout(10000);
-
 beforeAll(async () => {
   await utils.models.sequelize.sync({ force: true, match: /test$/ });
   await utils.seed.userDAO.save();
