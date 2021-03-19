0bd0a4d761efcf47060d137e745d2023075bf7fa
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2021-03-19 12:23:30 +0100

    2nd test fix travis

--- src/tests/setup.test.js
+++ src/tests/setup.test.js
@@ -3,6 +3,8 @@ import 'jest-extended';
 
 import utils from './utils';
 
+jest.setTimeout(10000);
+
 beforeAll(async () => {
   await utils.models.sequelize.sync({ force: true, match: /test$/ });
   await utils.seed.userDAO.save();
